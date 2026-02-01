bdd7a70141206db215b0a2c7d783358746ae1654
VolodymyrZin <vl.zinych@gmail.com>
2026-02-01 13:48:59 +0200

    added fixtures

--- tests/test_the_internet.py
+++ tests/test_the_internet.py
@@ -1,29 +1,10 @@
-from playwright.sync_api import sync_playwright
+def test_home_title(page):
+    # Перевіряємо заголовок сторінки
+    assert page.title() == "The Internet"
 
 
-def test_home_title():
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
-        page = browser.new_page()
-        page.goto("https://the-internet.herokuapp.com/")
-
-        # Перевіряємо заголовок сторінки
-        assert page.title() == "The Internet"
-
-        browser.close()
-
-
-def test_form_auth_link():
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
-        page = browser.new_page()
-        page.goto("https://the-internet.herokuapp.com/")
-
-        # Клікаємо на посилання "Form Authentication"
-        page.locator("text=Form Authentication").click()
-
-        # Перевіряємо URL нової сторінки
-        assert "login" in page.url
-
-        browser.close()
-
+def test_form_auth_link(page):
+    # Клікаємо на посилання "Form Authentication"
+    page.locator("text=Form Authentication").click()
+    # Перевіряємо URL нової сторінки
+    assert "login" in page.url
